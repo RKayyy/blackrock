d19750c79b205c91c9ebad46eb41f7afd204c02b
reitikakumar <reitika.kumar@gmail.com>
2024-07-30 20:49:25 +0530

    backend change

---
+++ frontend/src/pages/CompanyStocksTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanyStocksTable = () => {
+  return (
+    <div>
+      CompanyStocksTable
+    </div>
+  )
+}
+
+export default CompanyStocksTable
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CompanyStocksTable from './CompanyStocksTable'
+import Dashboard from './Dashboard'
+
+const Home = () => {
+  return (
+    <div>
+      <Dashboard />
+      <CompanyStocksTable />
+    </div>
+  )
+}
+
+export default Home
