1d4b9c633666753b9c43fd9670e463de7ee2e7de
Sam Vasishat <svasishat2003@gmail.com>
2024-07-30 19:15:25 +0530

    Logo

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react'
 import Create from './Create'
+import Header from './pages/components/header';
+
 
 const Home = () => {
 
     const [todos, setTodos] = useState([]);
   return (
+    <>
+    <Header/>
     <div>
       <h2 className='text-blue-500'>ToDo List</h2>
       <Create />
         {
-            todos.length === 0 ? 
-            <div> No record </div> 
-            :
-
-            todos.map(todo => (
-                <div>
+          todos.length === 0 ? 
+          <div> No record </div> 
+          :
+          
+          todos.map(todo => (
+            <div>
                     {todo}
                 </div>
 
-            ))
+))
         }
     </div>
+</>
   )
 }
 
